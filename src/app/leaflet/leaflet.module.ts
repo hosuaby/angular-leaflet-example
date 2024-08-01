34e1b555697a2f6282112d321564a9fcdab8bce5
Alexei KLENIN <alexei.klenin@gmail.com>
2024-08-01 22:07:09 +0200

    Publish on GitHub pages

--- src/app/leaflet/leaflet.module.ts
+++ src/app/leaflet/leaflet.module.ts
@@ -4,6 +4,12 @@ import {MapComponent} from './components/map/map.component';
 import L from 'leaflet';
 import SmoothMarkerBouncing from 'leaflet.smooth_marker_bouncing';
 
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'assets/images/marker-icon-2x.png',
+  iconUrl: 'assets/images/marker-icon.png',
+  shadowUrl: 'assets/images/marker-shadow.png',
+});
+
 SmoothMarkerBouncing(L);
 
 @NgModule({
